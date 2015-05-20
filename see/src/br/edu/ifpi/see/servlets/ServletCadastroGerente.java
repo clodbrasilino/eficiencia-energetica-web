991825eccae227b496187bb59fdf4d1f3ee6ea71
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-05-20 12:17:10 -0300

    Implementação do ServletCadastroGerente

--- see/src/br/edu/ifpi/see/servlets/ServletCadastroGerente.java
+++ see/src/br/edu/ifpi/see/servlets/ServletCadastroGerente.java
@@ -1,10 +1,14 @@
 package br.edu.ifpi.see.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.edu.ifpi.see.dao.UsuarioDAO;
+import br.edu.ifpi.see.model.Usuario;
 
 /**
  * Servlet implementation class ServletCadastroGerente
@@ -22,15 +26,20 @@ public class ServletCadastroGerente extends HttpServlet {
     // TODO Implementar Servlet de Cadastrar Gerente
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String telefone = request.getParameter("telefone");
+		String senha = request.getParameter("senha");
+		String confirmacaoSenha = request.getParameter("confirmacaoSenha");
+		
+		if(senha.equals(confirmacaoSenha)){
+			UsuarioDAO dao = new UsuarioDAO();
+			dao.salvar(new Usuario(nome, endereco, email, telefone, senha, 2, null));
+		}
 	}
 
 }
