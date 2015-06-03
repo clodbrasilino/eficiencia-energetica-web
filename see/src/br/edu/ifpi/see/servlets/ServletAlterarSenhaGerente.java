81330c2350ee199ab11e14665841f93ffb7f46b0
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-06-03 09:33:10 -0300

    Conclusão do requisito "Alterar senha do Gerente".

---
+++ see/src/br/edu/ifpi/see/servlets/ServletAlterarSenhaGerente.java
@@ -0,0 +1,67 @@
+package br.edu.ifpi.see.servlets;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.edu.ifpi.see.dao.UsuarioDAO;
+import br.edu.ifpi.see.model.Usuario;
+import br.edu.ifpi.see.util.JPAUtil;
+
+/**
+ * Servlet implementation class ServletAlterarSenhaGerente
+ */
+public class ServletAlterarSenhaGerente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAlterarSenhaGerente() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String senhaAtual = request.getParameter("senhaAtual");
+		String novaSenha = request.getParameter("novaSenha");
+		String confirmacaoSenha = request.getParameter("confirmacaoSenha");
+		
+		Usuario u = (Usuario) request.getSession().getAttribute("usuario");
+		
+		if(senhaAtual.equals(u.getSenha())){
+			
+			if(confirmacaoSenha.equals(novaSenha)){
+				
+				UsuarioDAO dao = new UsuarioDAO();
+				
+				EntityManager em = JPAUtil.getEntityManager();
+				EntityTransaction et = em.getTransaction();
+				
+				et.begin();
+				u.setSenha(novaSenha);
+				dao.atualizar(u);
+				et.commit();
+				
+				response.getWriter().println("<script>alert('Senha alterada com sucesso!')</script>");
+				response.getWriter().println("<script>window.location.href='/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/alterarDados.jsp'</script>");
+			}else{
+				response.getWriter().println("<script>alert('As senhas informadas são diferentes!')</script>");
+				response.getWriter().println("<script>window.location.href='/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/alterarDados.jsp'</script>");
+			}
+		}else{
+			response.getWriter().println("<script>alert('Senha inválida!')</script>");
+			response.getWriter().println("<script>window.location.href='/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/alterarDados.jsp'</script>");
+		}
+		
+	}
+
+}
