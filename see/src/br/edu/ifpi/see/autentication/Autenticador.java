ea5f75447fbea87fdb807b7d3017433b85e77690
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-09-16 11:39:18 -0300

    Implementação do objeto que faz a autenticação de usuários no sistema

--- see/src/br/edu/ifpi/see/autentication/Autenticador.java
+++ see/src/br/edu/ifpi/see/autentication/Autenticador.java
@@ -3,16 +3,14 @@ package br.edu.ifpi.see.autentication;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.edu.ifpi.see.model.Usuario;
-
 public class Autenticador {
 	
-	public void autentica(Usuario u, HttpServletRequest request, HttpServletResponse response){
+	public void autentica(Autenticavel a, HttpServletRequest request, HttpServletResponse response){
 		
 		try{
-			if(u != null){
-				request.getSession().setAttribute("usuario", u);
-				response.sendRedirect(u.autentica());
+			if(a != null){
+				request.getSession().setAttribute("usuario", a);
+				response.sendRedirect(a.autentica());
 			}else{
 				request.getSession().setAttribute("mensagem","E-mail e/ou Senha inválido(s)!");
 				response.sendRedirect("/see/");
