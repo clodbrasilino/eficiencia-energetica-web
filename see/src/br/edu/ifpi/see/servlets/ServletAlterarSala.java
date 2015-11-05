fdf01341302d4b4bc9626212e5ba937bee927ec3
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-11-05 10:29:04 -0300

    Adicionando a mensagem de sucesso.

--- see/src/br/edu/ifpi/see/servlets/ServletAlterarSala.java
+++ see/src/br/edu/ifpi/see/servlets/ServletAlterarSala.java
@@ -17,25 +17,15 @@ import br.edu.ifpi.see.model.MicroControlador;
 import br.edu.ifpi.see.model.Sala;
 import br.edu.ifpi.see.model.Usuario;
 import br.edu.ifpi.see.util.JPAUtil;
+import br.edu.ifpi.see.util.Message;
 
-/**
- * Servlet implementation class ServletAlterarSala
- */
 public class ServletAlterarSala extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ServletAlterarSala() {
         super();
     }
     
-    // TODO Implementar Servlet de Alterar Sala
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		long id = Integer.parseInt(request.getParameter("id"));
@@ -55,37 +45,7 @@ public class ServletAlterarSala extends HttpServlet {
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		/*long id = Integer.parseInt(request.getParameter("id"));
-		String numero = request.getParameter("numero");
-		String descricao = request.getParameter("descricao");
-		int pavimento = Integer.parseInt(request.getParameter("pavimento"));
-		
-//		EntityTransaction et = JPAUtil.getTransaction();
-		EntityTransaction et = ((EntityManager) getServletContext().getAttribute("em")).getTransaction();
-		
-		et.begin();
-		SalaDAO dao = new SalaDAO();
-		
-		Sala s = dao.pesquisar(id);
-		
-		s.setNumero(numero);
-		s.setDescricao(descricao);
-		s.setPavimento(pavimento);
-		
-		dao.atualizar(s);
-		et.commit();
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<script>");
-		out.println("alert('Sala atualizada com sucesso!')");
-		out.println("document.location.href='/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/gerente.jsp'");
-		out.println("</script>");*/
 		
 		String salvar = request.getParameter("salvar");
 		String add = request.getParameter("add");
@@ -136,14 +96,17 @@ public class ServletAlterarSala extends HttpServlet {
 			}
 			
 			// Mensagem
-			PrintWriter out = response.getWriter();
+			/*PrintWriter out = response.getWriter();
 			out.println("<script>");
 			out.println("alert('Sala atualizada com sucesso!');");
-			out.println("</script>");
+			out.println("</script>");*/
 			
 			// Remove o objeto sala da sessão e direciona para a página de gerente
 			request.getSession().removeAttribute("sala");
-			response.sendRedirect("/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/gerente.jsp");
+			
+			Message msg = new Message(request, response, "Sala atualizada com sucesso!", "/JSP/gerente/gerente.jsp");
+			msg.show();
+			//response.sendRedirect("/"+getServletContext().getInitParameter("app-name")+"/JSP/gerente/gerente.jsp");
 		
 		/* Este bloco de código é executado quando o usuário clica no botão 'Adicionar' e chama a página de cadastro de micro controladores */
 		}else if(add != null){
